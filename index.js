6ba727478e615227dd72873f7f629b613c1b3bc1
jadeleafmoon <jadeleafmoon@gmail.com>
2022-05-21 03:09:45 +0900

    feat: add /item as test

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 require('dotenv').config();
 const express = require('express');
+const { restart } = require('nodemon');
 const app = express();
 const PORT = process.env.PORT || 6060;
 
 const config = require('./knexfile');
-const knex = require('knex')(config)[process.env.NODE_ENV || 'development'];
+// const knex = require('knex')(config)[process.env.NODE_ENV || 'development'];
+const knex = require('knex')(config);
 
 app.get('/hello', (req, res) => {
-    
-    res.send('Hello world !');
-    // knex.raw('SELECT VERSION()').then((data) => {
-    //     console.log('🔥 version', data);
-        
-    // });
-	
+	res.send('Hello world !');
+	// knex.raw('SELECT VERSION()').then((data) => {
+	//     console.log('🔥 version', data);
+
+	// });
 });
 
 app.get('/items', (req, res) => {
-	knex('items').orderBy('id', 'desc').returning('*').then((data) => {
-		console.log("💜 data", data);
-		res.send(data);
-		
+	knex.raw('SELECT version()').then((data) => {
+		console.log('🔥 version', data.rows[0].version);
+        res.send("Working!");
 	});
+	// knex('items').orderBy('id', 'desc').returning('*').then((data) => {
+	// 	console.log("💜 data", data);
+	// 	res.send(data);
+
+	// });
 });
 
 app.get('/', (req, res) => {
