5b3967933f996db38e6aa932330dd4591e47e17d
jadeleafmoon <jadeleafmoon@gmail.com>
2022-05-21 02:14:36 +0900

    feat: add hello endpoint

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port = 6060;
 
+app.get('/hello', (req, res) => {
+	res.send('Hello world !');
+});
+
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Hi there! You have reached / !');
+});
+
+app.get('/*', (req, res) => {
+	res.send('Hi there! That endpoint does not exist !');
 });
 
 app.listen(port, () => {
