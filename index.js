f88b0ef865b4afa898f8c4180a5e28bd9b850214
jadeleafmoon <jadeleafmoon@gmail.com>
2022-05-21 02:25:19 +0900

    feat: add process.env.PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 6060;
+const PORT = process.env.PORT || 6060;
 
 app.get('/hello', (req, res) => {
 	res.send('Hello world !');
