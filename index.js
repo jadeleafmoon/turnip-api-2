32381eb3343b461cdf19d588a50e3606e0a5d36f
jadeleafmoon <jadeleafmoon@gmail.com>
2022-05-21 04:12:14 +0900

    feat: add post patch and delete /item

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const config = require('./knexfile');
 const knex = require('knex')(config);
 
 app.get('/hello', (req, res) => {
-	res.send('Hello world !');
+	res.send('Hello world from TURNIP API 2!');
 	// knex.raw('SELECT VERSION()').then((data) => {
 	//     console.log('🔥 version', data);
 
@@ -26,6 +26,40 @@ app.get('/items', (req, res) => {
 	knex('items').orderBy('id', 'desc').returning('*').then((data) => {
 		console.log('💜 data', data);
 		res.send(data);
+	});
+});
+
+app.post('/items', (req, res) => {
+	const newItem = req.body;
+	console.log('🔥 POST /items req', newItem);
+
+	knex('items').insert(newItem).returning('*').then((result) => {
+		res.status(201).send(`The product ${result[0].name} has been added`);
+	});
+});
+
+app.patch('/items/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const itemUpdates = req.body;
+
+	knex('items')
+		.where('id', '=', id)
+		.update(itemUpdates)
+		.returning('*')
+		.then((result) => {
+			res
+				.status(200)
+				.send(`The product ${itemUpdates.name} with id ${id} has been updated`);
+		});
+});
+
+app.delete('/items/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+
+	knex('items').where('id', '=', id).del().returning('*').then((result) => {
+		res
+			.status(200)
+			.send(`The product ${result[0].name} with id ${id} has been deleted`);
 	});
 });
 
