f1d2689fcaa40e41c6555ab10a27cbc59dab5e8c
jadeleafmoon <jadeleafmoon@gmail.com>
2022-07-11 23:56:51 +0900

    feat: add imageUrl column

---
+++ migrations/20220711144146_add_url.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('items', function(table) {
+    table.string('imageUrl').defaultTo("");
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('items', function(table) {
+    table.dropColumn('imageUrl');
+	});
+};
